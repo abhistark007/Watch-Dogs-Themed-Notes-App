16d787c1e6571624362f0b3a39eebc89ffe0fe91
Abhi Kumar <abhi.kumar.dev7@gmail.com>
2025-06-02 18:16:50 +0530

    doneee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { desc, eq } from "drizzle-orm";
 export default async function Home() {
   const user = await getUser();
   const _notes = await db.select().from(notes).where(eq(notes.userId,user.id)).orderBy(desc(notes.updatedAt))
+
+
  
   return <div className="min-h-screen w-full flex flex-col items-center px-4 pb-24 bg-gray-400">
     <Header />
